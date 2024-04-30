52ce8cbc7ed8e0ad2615b7bca73e45ea2ad82fd2
Maria Fernanda <132359722+Marifer150805@users.noreply.github.com>
2024-04-30 15:38:06 -0300

    Componente

---
+++ src/css/botaoStyle.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    button: {
+     paddingVertical: 20,
+     paddingHorizontal: 140,
+     borderRadius: 5,
+     marginVertical: 10,
+    },
+    buttonPressed: {
+        backgroundColor: '#1577F2'
+    },
+    buttonText: {
+        color: '#010101',
+        fontSize: 16,
+        textAlign: 'center',
+    }
+  });
+  
